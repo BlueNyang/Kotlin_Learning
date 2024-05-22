8183d7d418f61f2439275eacf1837d340630175f
Ahn GyuTae <xpsj20@gmail.com>
2024-05-22 21:49:47 +0900

    ::Syntax::

--- src/ch03_helloworld/HelloWorld.java
+++ src/ch03_helloworld/HelloWorld.java
@@ -1,4 +1,4 @@
-package ch03;
+package ch03_helloworld;
 
 public class HelloWorld {
     public static void main(String[] args) {
---
+++ src/ch04_syntax/syntax.kt
@@ -0,0 +1,14 @@
+package ch04_syntax
+
+fun main(args: Array<String>) {
+    print("Hello\n")
+    print("World\n\n")
+    println("Hello")
+    println("World");
+    // kotlin can use semicolon, but it is not required
+
+    // Quotes
+    println(" \"Hello World!\" ")
+    println(" 'Hello World!' ")
+    println(" \'Hello World!\' ")
+}
